b0957e2c0d506f9b46a5d531dec67a2d21e0bf93
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-11 19:06:05 +0300

    задача 2

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 // №1
 //
 // Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
+//
+// function all(num) {
+//     if(num < 0) {
+//         console.log('число отрицательное')
+//     }else if(num > 0){
+//         console.log('число положительное')
+//     }else{
+//         console.log('это не число')
+//     }
+// }
+// const a = prompt('напиши число')
+// all(a)
 
-function all(num) {
-    if(num < 0) {
-        console.log('число отрицательное')
-    }else if(num > 0){
-        console.log('число положительное')
-    }else{
-        console.log('это не число')
-    }
+// №2
+//
+// Дана строка. Выведите в консоль длину этой строки.
+function string(str) {
+
+    const strech2 = str.length -1
+    console.log(strech2)
+
 }
-const a = prompt('напиши число')
-all(a)
+const strech = prompt('Выведи строку ввиде имени');
+string(strech)
