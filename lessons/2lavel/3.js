73cb78d160bd29e8de221f1c5b1e97d384c83868
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-14 22:20:47 +0300

    зад main1

--- lessons/2lavel/3.js
+++ lessons/2lavel/3.js
@@ -1,28 +1,8 @@
 // №3
 //
 // Дано число. Выведите в консоль сумму первой и последней цифры этого числа.
-// function number(num) {
-//     num = "" + num
-//     return +num[0] + (+num[num.length - 1]);
-//
-// }
-//
-// console.log(number(23));
-// let num = 26789798989;
+function number(num) {
+    let a = num.toString()
 
-// let num2 = num.toString().split("")
-// let num3 = num2[2];
-// let num4 = num2[0];
-//
-// let resalt = +num3 + +num4;
-// console.log(resalt)
-
-let num = 23;
-let num2 = num.at(0).toUpperCase();
-console.log(num2);
-
-
-// let num3 = num.toUpperCase().at(num.length - 1);
-//
-// let resalt = num3 + num2;
-// console.log(resalt)
+}
+console.log(number(26579378));
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+//первая буква всегда большая
+function capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1)
+}
 
-
-
-
-
+    console.log(capitalizeWord("слон"))
 
 
 
