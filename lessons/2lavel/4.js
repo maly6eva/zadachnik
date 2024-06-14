434702e2ada90d22a5fd8107aba91885c469df46
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-14 23:43:21 +0300

    2/4

---
+++ lessons/2lavel/4.js
@@ -0,0 +1,10 @@
+// №4
+//
+// Дано число. Выведите количество цифр в этом числе.
+function number(num) {
+    let a = " " + num
+    let b = a.length-1
+    return b
+
+}
+console.log(number(352));
