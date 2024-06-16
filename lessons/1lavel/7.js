1166ad62058648d470afcc6426f7ef058cf40d64
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-16 13:33:51 +0300

    1/7

---
+++ lessons/1lavel/7.js
@@ -0,0 +1,10 @@
+
+// 1
+//первая буква всегда большая
+// function capitalizeWord(word) {
+//     return word[0].toUpperCase() + word.slice(1)
+// }
+//
+//     console.log(capitalizeWord("слон"))
+
+
--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-//первая буква всегда большая
-// function capitalizeWord(word) {
-//     return word[0].toUpperCase() + word.slice(1)
-// }
-//
-//     console.log(capitalizeWord("слон"))
-//
-//
-
-// 2
-
 
 
 
