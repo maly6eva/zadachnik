5995165df9b96c3d1f121d61131104743a548bd7
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-16 12:11:01 +0300

    zad4

--- main.js
+++ main.js
@@ -25,19 +25,38 @@
 //     Также надо разрешить пользователю остановить процесс ввода, отправив пустую строку или нажав «Отмена». В этом случае функция должна вернуть null.
 
 
+// function readNumber() {
+//     let num;
+//
+//     do {
+//         num = prompt("Введите число", 0);
+//     } while ( !isFinite(num) );
+//
+//     if (num === null || num === '') return null;
+//
+//     return +num;
+// }
+//
+// alert(`Число: ${readNumber()}`);
 
-function readNumber() {
-    let num;
-    do {
-        num = prompt("Введите число", 0);
-    } while ( !isFinite(num) );
-    if (num === null || num === ''){
-        return null;
-    } {
-        return +num;
-    }
 
 
-}
-console.log(readNumber())
+// 4
+// реши задачу: реши задачу 2 + 3
+// function readNumber() {
+//     let num;
+// do {
+//     num = +prompt('реши задачу 2 + 3')
+//         if (num !== 5) {
+//             return " это не верно"
+//         }else if(num === 5){
+//             return num
+//     }
+//
+//     } while ( !isFinite(num) ){
+//
+//     }
+//
+// }
+// console.log(readNumber())
 
