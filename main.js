d3f2dd6545d3c724ec4ee92911d38976b4b2ec6e
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-19 23:10:30 +0300

    document

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-// 12
-// Даны два целых числа. Проверьте, что первое число без остатка делится на второе.
-function number(num1, num2) {
-    if (num1 % num2 === 0) {
-        return true;
-    }{
-        return false;
-    }
+
+// №1
+//
+// Выведите в консоль все целые числа от 1 до 100.
+
+// console.log(Math.trunc(1, 100));
+// Expected output: 13
 
 
-}
-console.log(number(24, 2));
+
+let alin = []
+
+alin[0]= prompt('Как тебя зовут?', '')
+alin[1]= prompt('Каая твоя фамилия', '')
+alin[2]= prompt('Cколько тебе лет', '')
+
+document.write(alin)
