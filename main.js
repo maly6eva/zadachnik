8bccfbcfab61f442761dc5fc9cd16d54790624d3
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-20 22:46:26 +0300

    все файлы готовы

--- main.js
+++ main.js
@@ -1,17 +1,2 @@
 
-// №1
-//
-// Выведите в консоль все целые числа от 1 до 100.
-
-// console.log(Math.trunc(1, 100));
-// Expected output: 13
-
-
-
-let alin = []
-
-alin[0]= prompt('Как тебя зовут?', '')
-alin[1]= prompt('Каая твоя фамилия', '')
-alin[2]= prompt('Cколько тебе лет', '')
-
-document.write(alin)
+//привет мирпше
