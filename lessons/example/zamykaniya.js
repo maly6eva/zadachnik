9b0c42074b6631ae3d037ba1f012414b7e1ef3c6
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-26 12:30:29 +0300

    example1

---
+++ lessons/example/zamykaniya.js
@@ -0,0 +1,7 @@
+// function zamykaniya(zam) {
+//     return function (z){
+//         return zam + z
+//     }
+// }
+// let za = zamykaniya(2)
+// console.log(za(4))
--- main.js
+++ main.js
@@ -1,8 +1,61 @@
 // №4
 //
 // Выведите в консоль все четные числа из промежутка от 1 до 100.
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+//
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+//
+//
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// }
+
+// function urlGenerator(domen) {
+//     return function(url) {
+//         return `https://${url}.${domen}`
+//     }
+// }
+//
+// const comUrl = urlGenerator('com')
+// const ruUrl = urlGenerator('ru')
+//
+// console.log(comUrl('google'))
+// console.log(comUrl('netflix'))
+// console.log(ruUrl('gmail'))
+
+
+function createFun(n) {
+    return function () {
+        console.log(100 + n)
     }
 }
+let a = createFun(5)
+a()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
