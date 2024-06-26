414a2c1d23a026dae3e07ddb04bc4db249b70ad1
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-26 15:56:52 +0300

    zam

--- lessons/example/schetchik.js
+++ lessons/example/schetchik.js
@@ -1,11 +1,29 @@
-function schetchik(schet) {
-    return function () {
-        return schet++
-    }
-}
+// function schetchik(schet) {
+//     return function () {
+//         return schet++
+//     }
+// }
+//
+// let a = schetchik(0)
+// console.log(a())
+// console.log(a())
+// console.log(a())
+// console.log(a())
 
-let a = schetchik(0)
-console.log(a())
-console.log(a())
-console.log(a())
-console.log(a())
+function creat() {
+    let cont = 0;
+    function incr() {
+        cont++
+    }
+
+    function log() {
+        let mes = `Count is ${cont}`;
+        console.log(mes);
+        console.log(mes);
+    }
+    return [incr, log]
+}
+const [incr, log] = creat()
+incr()
+incr()
+log()
--- lessons/example/zamykaniya.js
+++ lessons/example/zamykaniya.js
@@ -5,3 +5,12 @@
 // }
 // let za = zamykaniya(2)
 // console.log(za(4))
+
+
+const x = 1;
+const y = function () {
+    const i = 'Hi'
+    console.log(i)
+}
+//замыканием могут быть не только функции в функции,
+// замыкание может быть и в простом примере одной функции
