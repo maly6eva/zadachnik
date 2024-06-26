c421aee7e5ac1b2e2d179d25ea9df224f9aeb8ef
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-26 23:49:24 +0300

    function

---
+++ lessons/example/cykl.js
@@ -0,0 +1,7 @@
+// function getRandomInt(min, max) {
+//     for (let i = 0; i < max; i++) {
+//         console.log(min)
+//     }
+// }
+// getRandomInt('hi', 5)
+// getRandomInt('hi2', 9)
---
+++ lessons/example/function.js
@@ -0,0 +1,9 @@
+function ret(){
+    let num = 50;
+
+    //
+
+    return num
+}
+const a = ret()
+console.log(a)
