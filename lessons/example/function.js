917d74e2b1b69339c009bd014f92090474e7ea03
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-27 13:22:18 +0300

    function2

--- lessons/example/function.js
+++ lessons/example/function.js
@@ -1,9 +1,52 @@
-function ret(){
-    let num = 50;
+// function ret(){
+//     let num = 50;
+//
+//     //
+//
+//     return num
+// }
+// const a = ret()
+// console.log(a)
 
-    //
 
-    return num
+let cos = 23;
+let bos = 65;
+
+function num(a, b) {
+    return a + b;
 }
-const a = ret()
-console.log(a)
+console.log(num(5, 67))
+
+function add() {
+    return cos + bos
+}
+
+console.log(add())
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+
+        if (i === 3) return
+        console.log(i)
+    }
+    console.log('Done')
+}
+
+test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
