a6d0a9a33c50a2f40cb2fff5ded86de32bc701f2
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-30 00:11:24 +0300

    masiv

--- lessons/example/destructur.js
+++ lessons/example/destructur.js
@@ -1,17 +1,59 @@
-const option = {
-    name: 'test',
-    width: 1024,
-    height: 12,
-    colors: {
-        border: 'red',
-        bg: 'blue'
-    },
-    makeTest: function() {
-        console.log('test')
+// const option = {
+//     name: 'test',
+//     width: 1024,
+//     height: 12,
+//     colors: {
+//         border: 'red',
+//         bg: 'blue'
+//     },
+//     makeTest: function() {
+//         console.log('test')
+//     }
+// }
+// option.makeTest()
+// // console.log(Object.keys(option).length)
+// const { border, bg} = option.colors
+// console.log( border)
+
+
+
+
+
+//
+//
+const arr = [2, 4, 5]
+function array (a, b) {
+
+    for (let v of arr) {
+        return v
     }
 }
-option.makeTest()
-// console.log(Object.keys(option).length)
-const { border, bg} = option.colors
-console.log( border)
+const s = array([44, 8, 9], deep)
+console.log(s)
+function deep (n) {
+   return n * n
+}
+
+
+//
+// function five(n) {
+//     return n * 2
+// }
+//
+// let rez = array(v, five)
+// console.log(rez)
+
+// const arr = [2, 4, 5]
+// for (let value of arr) {
+//     console.log(value * 2)
+// }
+
+
+
+
+
+
+
+
+
 
---
+++ lessons/example/massiv.js
@@ -0,0 +1,38 @@
+// let arr = [8, 6, 5, 7, 12];
+//
+// arr[99] = 0
+// console.log(arr.length)
+// //length -последний элемент + 1
+//
+// arr.forEach(function(a, b, c) {
+//     // а - элемент, который мы перебираем
+//     // b - номер по порядку
+//     // c - ссылка на массив который мы перебираем
+//     console.log(`${b}: ${b} внутри ${c}`)
+//
+// })
+// break, continue не используются в методе forEach, в отличие от других циклов
+
+//2
+// const str = prompt("", "");
+// const products = str.split(", ")
+// products.sort() // отсортировать от меншему к большему
+//
+// console.log(products)
+// console.log(products.join('; '))
+
+
+//3
+// const arr = [ 3, 10, 2, 8]; // по возростанию первой цифры
+// arr.sort()
+// console.log(arr)
+
+
+//4
+// const arr = [ 3, 10, 2, 8]; // от меньшей к большей
+// arr.sort(com)
+// console.log(arr)
+//
+// function com(a, b) {
+//     return a - b
+// }
