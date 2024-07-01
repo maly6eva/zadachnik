7a788019317f18be11cb870134b45e1b2d74e6c7
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-01 12:51:19 +0300

    sobytiya

---
+++ lessons/example/sobutiya.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('.btn')
+const buttonTwo = document.querySelector('.btnTwo')
+
+
+const handleClick = () => {
+    console.log('click - 2')
+}
+
+button.addEventListener('click', handleClick);
+buttonTwo.addEventListener('click', handleClick);// повесить обработчик
+// buttonTwo.addEventListener('click', handleClick);
+// button.removeEventListener('click', handleClick)//поменять обработчик
