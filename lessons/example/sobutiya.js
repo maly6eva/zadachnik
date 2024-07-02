5fab9346649829f5e906a7e51e28c9bdedece493
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-02 18:10:39 +0300

    sobytiya

--- lessons/example/sobutiya.js
+++ lessons/example/sobutiya.js
@@ -10,3 +10,15 @@ button.addEventListener('click', handleClick);
 buttonTwo.addEventListener('click', handleClick);// повесить обработчик
 // buttonTwo.addEventListener('click', handleClick);
 // button.removeEventListener('click', handleClick)//поменять обработчик
+
+
+const buttons = document.querySelectorAll('.btn');
+
+
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        console.log('clicked!')
+    })
+})
