96a4940555289ab26b58d25a29cb316ed1432790
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-04 15:38:27 +0300

    project.rez

--- project/js/script.js
+++ project/js/script.js
@@ -24,3 +24,36 @@ const movieDB = {
     ]
 };
 
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list'),
+      promo = movieList.querySelectorAll('.promo__interactive-item')
+
+
+
+adv.forEach(item => {
+    item.remove();
+})
+
+genre.textContent = 'драма'
+
+poster.style.backgroundImage = 'url("img/bg.jpg")'
+
+promo.forEach(item => {
+    item.remove()            //или movieList.innerHTML
+})
+
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `<li class="promo__interactive-item"> ${i + 1} ${film}
+    <div class="delete"></div>
+`;
+})
+
+
+
+
