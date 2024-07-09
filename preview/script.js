1cc046eba63e4af55370360fed2bba101ff452ba
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-09 20:21:47 +0300

    this

--- preview/script.js
+++ preview/script.js
@@ -34,21 +34,136 @@
 
 
 //функции-конструкторы
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`)
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`)
+//     }
+// }
+//
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} ушел`)
+// }
+// const iv = new User('Iv', 23)
+//
+// iv.hello()
+// iv.exit()
+//
+// console.log(iv)
+
+
+//this - контескт вызова функции
+//
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//
+//     console.log(sum())
+// }
+// showThis(4, 5);
+//
+//
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this)
+//     }
+// }
+// obj.sum()
+//
+//
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//
+//     }
+// let iv = new User('Iv', 23)
+
+
+// function sayName(surname ) {
+//     console.log(this);
+//     console.log((this.name + surname))
+// }
+
+// const user = {
+//     name: 'Join'
+// };
+//
+// sayName.call(user, 'Smit');
+// sayName.apply(user, ['Smith'])
+//
+//
+// function count(num) {
+//     return this*num
+// }
+// const doble = count.bind(2);
+// console.log(doble(3))
+// console.log(doble(13))
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', function() {
+    console.log(this)
+})
+
+const obj = {
+    num: 5,
+    sayNum: function(){
+        const say = () => {
+            console.log((this))
+        }
+        say();
     }
+
 }
+obj.sayNum();
 
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`)
-}
-const iv = new User('Iv', 23)
 
-iv.hello()
-iv.exit()
+const double = (a) => {
+    return a * 2;
+};
+console.log(double(4))
 
-console.log(iv)
+
+
+
+
+
+//1) Обычная функция: this = window, но если use
+//2) Контекст у методов обьекта - сам обьект
+//3) this в конструкторах и классах - это новый экземпляр обьектов
+//4)ручная привязка this: coll, apply, bind
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- preview2/script.js
+++ preview2/script.js
@@ -1,34 +1,59 @@
 'use strict';
-//верстка тоже
-const box = document.querySelector('.box');
+// //верстка тоже
+// const box = document.querySelector('.box');
+// const btn = document.querySelector('button')
+//
+// // const width = box.clientWidth;
+// // const height = box.clientHeight;
+//
+// // const width = box.offsetWidth;
+// // const height = box.offsetHeight;
+//
+// const width = box.scrollWidth;
+// const height = box.scrollHeight;
+//
+// console.log(width, height)
+//
+//
+// btn.addEventListener('click', () => {
+//     // box.style.height = box.scrollHeight + 'px;'
+//
+//     console.log(box.scrollTop);
+// })
+//
+// console.log(box.getBoundingClientRect());
+// console.log(box.getBoundingClientRect().top);
+//
+// const style = window.getComputedStyle(box);
+// console.log(style)
+//
+// console.log(style.display)
+//
+// console.log(document.documentElement.clientWidth)
+//
+// console.log(document.documentElement.scrollTop)
+
+
 const btn = document.querySelector('button')
 
-// const width = box.clientWidth;
-// const height = box.clientHeight;
+btn.addEventListener('click', (e) => {
+   e.target.style.background = "red"
+});
 
-// const width = box.offsetWidth;
-// const height = box.offsetHeight;
+const obj = {
+    num: 5,
+    sayNum: function(){
+        const say = () => {
+            console.log((this.num))
+        };
+        say();
+    }
 
-const width = box.scrollWidth;
-const height = box.scrollHeight;
-
-console.log(width, height)
+}
+obj.sayNum();
 
 
-btn.addEventListener('click', () => {
-    // box.style.height = box.scrollHeight + 'px;'
-
-    console.log(box.scrollTop);
-})
-
-console.log(box.getBoundingClientRect());
-console.log(box.getBoundingClientRect().top);
-
-const style = window.getComputedStyle(box);
-console.log(style)
-
-console.log(style.display)
-
-console.log(document.documentElement.clientWidth)
-
-console.log(document.documentElement.scrollTop)
+const double = (a) => {
+    return a * 2;
+};
+console.log(double(4))
