2c4d7a784306761189967b1aaf2abb1b9b35d6da
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-09 11:29:58 +0300

    style.css

--- preview2/script.js
+++ preview2/script.js
@@ -1,2 +1,34 @@
 'use strict';
+//верстка тоже
+const box = document.querySelector('.box');
+const btn = document.querySelector('button')
 
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
+
+console.log(width, height)
+
+
+btn.addEventListener('click', () => {
+    // box.style.height = box.scrollHeight + 'px;'
+
+    console.log(box.scrollTop);
+})
+
+console.log(box.getBoundingClientRect());
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+console.log(style)
+
+console.log(style.display)
+
+console.log(document.documentElement.clientWidth)
+
+console.log(document.documentElement.scrollTop)
