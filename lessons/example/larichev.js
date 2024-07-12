13894fd411bad1177a265cff00833b5b09c4631e
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-12 19:57:35 +0300

    fetch

--- lessons/example/larichev.js
+++ lessons/example/larichev.js
@@ -121,6 +121,32 @@
 // Promise.resolve('Success').then(data => console.log(data))
 
 
+//Создание функции Fetch
+
+function myFetch(url) {
+    return new Promise ((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.open("GET", url)
+        request.send();
+
+        request.addEventListener('load', function() {
+            if(this.status > 400) {
+                reject(new Error(this.status))
+            }
+            resolve(this.responseText);
+        })
+        request.addEventListener('error', function() {
+            reject(new Error(this.status))
+        })
+        request.addEventListener('timeout', function() {
+            reject(new Error('Timeout'))
+        })
+    })
+}
+myFetch('https://jsonplaceholder.typicode.com/posts')
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+
 
 
 
